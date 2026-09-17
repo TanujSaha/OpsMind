23f2e74d9dbf3c4ef76c3fdaa17dc004a533b926
TanujSaha <Sahatanuj41@gmail.com>
2026-09-17 06:19:19 +0530

    fix env var

--- backend/src/server.js
+++ backend/src/server.js
@@ -20,7 +20,7 @@ app.use('/api/dashboard', dashboardRoutes);
 // Error Middleware
 app.use(errorHandler);
 
-mongoose.connect(process.env.MONGODB_URI)
+mongoose.connect(process.env.MONGO_URI)
   .then(() => {
     console.log('✅ MongoDB Connected');
     app.listen(PORT, () => console.log(`🚀 Backend running on port ${PORT}`));
